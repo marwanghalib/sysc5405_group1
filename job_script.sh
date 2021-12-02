5b65b5a3d912553ada70cf6e825f70780aefdb8b
Marwan Ghalib <mghalib@cedar5.cedar.computecanada.ca>
2021-12-01 18:19:13 -0800

    compute Canada

--- job_script.sh
+++ job_script.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --mem=4G
-#SBACTH --time=2:00:00
+#SBATCH --time=5:00:00
 #SBATCH --ntasks-per-node=4
 #SBATCH --mail-user=marwanghalib@cmail.carleton.ca
 #SBATCH --mail-type=ALL
+
+cd /project/def-jrgreen/mghalib/group_1/sysc5405_group1
+module load
+source ~/py38-cc/bin/activate
 
 
 python3 lstm_RNN_CV.py
